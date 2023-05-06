89cf5624483956e6d278355c599bcb9fc43dcb81
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-06 16:10:56 +0900

    TodoList 구현 완료

--- app/src/main/java/kgb/plum/todolist/MainActivity.kt
+++ app/src/main/java/kgb/plum/todolist/MainActivity.kt
@@ -1,8 +1,10 @@
 package kgb.plum.todolist
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -10,16 +12,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kgb.plum.todolist.ui.theme.TodoListTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,6 +49,22 @@ fun TodoList(){
         setText("")
     }
 
+    val onToggle: (Int, Boolean) -> Unit = {key, checked ->
+        val index = todoList.indexOfFirst { it.key == key }
+        todoList[index] = todoList[index].copy(done = checked)
+    }
+
+    val onEdit: (Int, String) -> Unit = {key, content ->
+        val index = todoList.indexOfFirst { it.key == key }
+        todoList[index] = todoList[index].copy(content = content)
+    }
+
+    val onDelete : (Int) -> Unit = {key ->
+        val index = todoList.indexOfFirst {it.key == key}
+        todoList.removeAt(index)
+    }
+
+
     Scaffold{
         Column(modifier = Modifier.padding(4.dp)){
             InputData(
@@ -58,7 +74,12 @@ fun TodoList(){
             )
             LazyColumn {
                 items(todoList, key = {it.key}) { todoData ->
-                    Text(todoData.content)
+                    TodoItem(
+                        todoData = todoData,
+                        onToggle = onToggle,
+                        onEdit = onEdit,
+                        onDelete = onDelete
+                    )
                 }
             }
         }
@@ -99,6 +120,103 @@ fun InputData(
     }
 }
 
+@Composable
+fun TodoItem(
+    todoData: TodoData,
+    onToggle: (Int, Boolean) -> Unit = {_,_ -> },
+    onEdit: (Int, String) -> Unit = {_,_ ->},
+    onDelete: (Int) -> Unit = {_ -> }
+) {
+    var isEditing by remember {mutableStateOf(false)}
+
+    Card(
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Crossfade(targetState = isEditing) {
+            when(it) {
+                false -> {
+                    Row(
+                        modifier = Modifier.padding(6.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(
+                            text = todoData.content,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Text(
+                            text = "완료"
+                        )
+                        Checkbox(
+                            checked = todoData.done,
+                            onCheckedChange = {
+                                onToggle(todoData.key, it)
+                            }
+                        )
+                        Button(
+                            onClick = {
+                                isEditing = true
+                            },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                            modifier = Modifier.background(color = Color.DarkGray, shape = RoundedCornerShape(6.dp))
+                        ) {
+                            Text(
+                                text = "수정",
+                                color = Color.White,
+                                fontSize = 12.sp
+                                )
+                        }
+                        Spacer(modifier = Modifier.size(6.dp))
+                        Button(
+                            onClick = {
+                                onDelete(todoData.key)
+                            },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                            modifier = Modifier
+                                .background(
+                                    color = Color.DarkGray,
+                                    shape = RoundedCornerShape(6.dp)
+                                )
+                                .wrapContentSize(unbounded = true)
+                        ) {
+                            Text(
+                                text = "삭제",
+                                color = Color.White,
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
+                }
+                true -> {
+                    val (newText, setText) = remember { mutableStateOf(todoData.content) }
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = setText,
+                            modifier = Modifier.weight(1f),
+                            colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = Color.DarkGray)
+                        )
+                        Spacer(modifier = Modifier.size(8.dp))
+                        Button(
+                            onClick = {
+                                onEdit(todoData.key, newText)
+                                isEditing = false
+                            },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                            modifier = Modifier.background(color = Color.DarkGray, shape = RoundedCornerShape(6.dp))
+                        ) {
+                            Text(
+                                text = "수정",
+                                color = Color.White,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
@@ -120,6 +238,6 @@ fun InputDataPreview(){
 @Composable
 fun TodoData(){
     TodoListTheme{
-
+        TodoItem(TodoData(1,"TodoList 구현하기"))
     }
 }
